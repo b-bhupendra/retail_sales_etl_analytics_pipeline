a33059562934a6e1b1058bf48f4178d11d5da59a
b-bhupendra <100297227+b-bhupendra@users.noreply.github.com>
2026-07-31 20:14:13 +0530

    Refactor data loading and introduce pipeline function
    Refactor load_to_db function and add run_pipeline function to streamline data loading process.

--- src/load.py
+++ src/load.py
@@ -5,11 +5,14 @@ from src.transform import transform_data
 DB_PATH = os.path.join(os.path.dirname(__file__), "..", "data", "retail_sales.db")
 
 def load_to_db(df):
-    os.makedirs(os.path.dirname(DB_PATH), exist_ok=True)
-    conn = sqlite3.connect(DB_PATH)
-    df.to_sql("df_orders", conn, if_exists="replace", index=False)
-    conn.close()
+        os.makedirs(os.path.dirname(DB_PATH), exist_ok=True)
+        conn = sqlite3.connect(DB_PATH)
+        df.to_sql("df_orders", conn, if_exists="replace", index=False)
+        conn.close()
+
+def run_pipeline():
+        df = transform_data(extract_data())
+        load_to_db(df)
 
 if __name__ == "__main__":
-    df = transform_data(extract_data())
-    load_to_db(df)
+        run_pipeline()
